f4aaa83f18006a8b1a56f3a39a34b8037edee093
Aleksandr Mishin <mishin.aa@phystech.edu>
2023-09-18 23:01:20 +0300

    [.clang-format]

--- include/Memory.h
+++ include/Memory.h
@@ -2,54 +2,52 @@
 #define MEMORY_H
 
 #include <inttypes.h>
+
 #include <unordered_map>
 
-#define PAGE_BYTESIZE 			4096		    // 4 KiB
-#define PHYS_MEMORY_BYTESIZE 	8589934592 	    // 8 GiB
-#define VIRT_MEMORY_BYTESIZE 	8589934592      // 8 GiB
-#define PHYS_PAGE_COUNT 	    PHYS_MEMORY_BYTESIZE / PAGE_BYTESIZE
+#define PAGE_BYTESIZE 4096               // 4 KiB
+#define PHYS_MEMORY_BYTESIZE 8589934592  // 8 GiB
+#define VIRT_MEMORY_BYTESIZE 8589934592  // 8 GiB
+#define PHYS_PAGE_COUNT PHYS_MEMORY_BYTESIZE / PAGE_BYTESIZE
 
-#define ADDRESS_PAGE_NUM_SHIFT 		12
-#define ADDRESS_PAGE_OFFSET_MASK 	0xFFF
+#define ADDRESS_PAGE_NUM_SHIFT 12
+#define ADDRESS_PAGE_OFFSET_MASK 0xFFF
 
-#define STACK_BYTESIZE          1048576     // 1 MiB
-#define DEFAULT_STACK_ADDRESS   VIRT_MEMORY_BYTESIZE - STACK_BYTESIZE
+#define STACK_BYTESIZE 1048576  // 1 MiB
+#define DEFAULT_STACK_ADDRESS VIRT_MEMORY_BYTESIZE - STACK_BYTESIZE
 
 namespace RISCV {
 
-
 struct Page {
-	uint8_t memory[PAGE_BYTESIZE];
+  uint8_t memory[PAGE_BYTESIZE];
 };
-
 
 class MMU {
+ private:
+  std::unordered_map<uint32_t, Page*> allocatedPhysPages_;
+  uint64_t stackAddress_ = DEFAULT_STACK_ADDRESS;
 
-private:
-	std::unordered_map<uint32_t, Page*> allocatedPhysPages_;
-    uint64_t stackAddress_ = DEFAULT_STACK_ADDRESS;
+  bool allocatePage(const uint32_t pageNum);
 
-	bool allocatePage(const uint32_t pageNum);
+ public:
+  uint64_t getStackAddress() const;
 
-public:
-    uint64_t getStackAddress() const;
+  bool load8(const uint64_t addr, uint8_t* value) const;
+  bool load16(const uint64_t addr, uint16_t* value) const;
+  bool load32(const uint64_t addr, uint32_t* value) const;
+  bool load64(const uint64_t addr, uint64_t* value) const;
 
-    bool load8 (const uint64_t addr,  uint8_t* value) const;
-    bool load16(const uint64_t addr, uint16_t* value) const;
-    bool load32(const uint64_t addr, uint32_t* value) const;
-    bool load64(const uint64_t addr, uint64_t* value) const;
+  bool store8(const uint64_t addr, uint8_t value);
+  bool store16(const uint64_t addr, uint16_t value);
+  bool store32(const uint64_t addr, uint32_t value);
+  bool store64(const uint64_t addr, uint64_t value);
 
-    bool store8 (const uint64_t addr,  uint8_t value);
-    bool store16(const uint64_t addr, uint16_t value);
-    bool store32(const uint64_t addr, uint32_t value);
-    bool store64(const uint64_t addr, uint64_t value);
+  bool loadElfFile(const std::string& filename, uint64_t* pc);
 
-    bool loadElfFile(const std::string& filename, uint64_t* pc);
-
-    MMU();
-    ~MMU();
+  MMU();
+  ~MMU();
 };
 
-}
+}  // namespace RISCV
 
-#endif // MEMORY_H
+#endif  // MEMORY_H
--- src/risc-v.cpp
+++ src/risc-v.cpp
@@ -1,31 +1,29 @@
-#include <stdlib.h> 
+#include <stdlib.h>
 
 #include "../include/CpuRV.h"
 
+int main(int argc, char* argv[]) {
+  if (argc < 2) {
+    fprintf(stdout, "Usage: %s <elf_filename>\n", argv[0]);
+    return -1;
+  }
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2) {
-        fprintf(stdout, "Usage: %s <elf_filename>\n", argv[0]);
-        return -1;
-    }
+  RISCV::CpuRV CPU;
+  CPU.loadElfFile(argv[1]);
 
-    RISCV::CpuRV CPU;
-    CPU.loadElfFile(argv[1]);
+  RISCV::EncodedInstruction encInstr;
+  RISCV::DecodedInstruction decInstr;
 
-    RISCV::EncodedInstruction encInstr;
-    RISCV::DecodedInstruction decInstr;
+  while (true) {
+    if (CPU.getPC() == 0)
+      break;
 
-    while (true) {
-        if (CPU.getPC() == 0)
-            break;
+    CPU.fetch(encInstr);
+    CPU.decode(encInstr, decInstr);
+    CPU.execute(decInstr);
+  }
 
-        CPU.fetch(encInstr);
-        CPU.decode(encInstr, decInstr);
-        CPU.execute(decInstr);
-    }
+  printf("\nInterpreting ELF file %s has finished\n", argv[1]);
 
-    printf("\nInterpreting ELF file %s has finished\n", argv[1]);
-
-    return 0;
+  return 0;
 }
