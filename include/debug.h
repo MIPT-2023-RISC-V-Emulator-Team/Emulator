7b4dd3eba7fb7ff6d0cd72f37afadc601991cefa
Ivan Panferov <panferov.ia@phystech.edu>
2023-09-24 13:33:49 +0300

    Added macross
    Signed-off-by: Ivan Panferov <panferov.ia@phystech.edu>

---
+++ include/debug.h
@@ -0,0 +1,27 @@
+#ifndef INCLUDE_DEBUG_H
+#define INCLUDE_DEBUG_H
+
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#include <cerrno>
+#include <cstring>
+#include <iostream>
+
+namespace RISCV::debug {
+[[noreturn]] void AssertionFail(const char* expr,
+                                const char* file,
+                                unsigned line,
+                                const char* function) {
+  int errnum = errno;
+  std::cerr << "ASSERTION FAILED: " << expr << std::endl;
+  std::cerr << "IN " << file << ":" << std::dec << line << ": " << function << std::endl;
+  if (errnum != 0) {
+    std::cerr << "ERRNO: " << errnum << " (" << std::strerror(errnum) << ")" << std::endl;
+  }
+  std::abort();
+}
+
+}  // namespace RISCV::debug
+
+#endif  // INCLUDE_DEBUG_H
