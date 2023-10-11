f6b21537d136f89907c8a4cbfff5efb41e4fda23
OAMichael <mixa.ovsyann@mail.ru>
2023-10-11 14:53:48 +0300

    Hotfix for time measurement

--- src/risc-v.cpp
+++ src/risc-v.cpp
@@ -22,40 +22,23 @@ int main(int argc, char* argv[]) {
     uint64_t decodeMicroseconds = 0;
     uint64_t executeMicroseconds = 0;
 
+    auto executeStart = std::chrono::high_resolution_clock::now();
     while (true) {
         if (CPU.getPC() == 0)
             break;
 
-        auto fetchStart = std::chrono::high_resolution_clock::now();
         CPU.fetch(encInstr);
-        auto fetchEnd = std::chrono::high_resolution_clock::now();
-        fetchMicroseconds +=
-            std::chrono::duration_cast<std::chrono::microseconds>(fetchEnd - fetchStart).count();
-
-        auto decodeStart = std::chrono::high_resolution_clock::now();
         CPU.decode(encInstr, decInstr);
-        auto decodeEnd = std::chrono::high_resolution_clock::now();
-        decodeMicroseconds +=
-            std::chrono::duration_cast<std::chrono::microseconds>(decodeEnd - decodeStart).count();
-
-        auto executeStart = std::chrono::high_resolution_clock::now();
         CPU.execute(decInstr);
-        auto executeEnd = std::chrono::high_resolution_clock::now();
-        executeMicroseconds +=
-            std::chrono::duration_cast<std::chrono::microseconds>(executeEnd - executeStart)
-                .count();
 
         ++instrCount;
     }
+    auto executeEnd = std::chrono::high_resolution_clock::now() - executeStart;
 
     printf("\nInterpreting ELF file %s has finished. Instruction count: %ld. AVG MIPS: %lf\n",
            argv[1],
            instrCount,
-           static_cast<float>(instrCount) /
-               (fetchMicroseconds + decodeMicroseconds + executeMicroseconds));
-    printf("Fetch:    %ld ms\n", fetchMicroseconds);
-    printf("Decode:   %ld ms\n", decodeMicroseconds);
-    printf("Execute:  %ld ms\n", executeMicroseconds);
+           static_cast<float>(instrCount) / (std::chrono::duration_cast<std::chrono::microseconds>(executeEnd).count()));
 
     return 0;
 }
