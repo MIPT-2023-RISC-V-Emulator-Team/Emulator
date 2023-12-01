3ac8f1c5cc4b1cfefca56ef5e167fe1df065a54c
OAMichael <mixa.ovsyann@mail.ru>
2023-12-01 18:18:31 +0300

    Added syscalls, refactored MMU

---
+++ test/scan.c
@@ -0,0 +1,22 @@
+#include "util.h"
+
+
+int main() {
+    puts("Enter a message:");
+
+    char c;
+    while ((c = getchar()) != EOF && c != '\n') {
+        putchar(c);
+    }
+    putchar('\n');
+
+    puts("Enter another message:");
+    char str[1024];
+    gets(str);
+    puts(str);
+
+    puts("Enter yet another message:");
+    scanf("%s", str);
+    puts(str);
+    return 0;
+}
