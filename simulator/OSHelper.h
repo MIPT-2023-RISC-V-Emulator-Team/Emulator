ea2994ca28f313ef16a469bd1e00469160355dad
OAMichael <mixa.ovsyann@mail.ru>
2023-12-05 23:34:52 +0300

    Added argc, argv, envp support. Merged ElfLoader, stack allocation and command line arguments initializing into OSHelper. Added test for argv. Fixed double host instruction count ending

---
+++ simulator/OSHelper.h
@@ -0,0 +1,37 @@
+#ifndef OS_HELPER_H
+#define OS_HELPER_H
+
+#include "simulator/Hart.h"
+#include "simulator/memory/Memory.h"
+
+namespace RISCV {
+
+class OSHelper final {
+private:
+    static OSHelper* instancePtr;
+    OSHelper() = default;
+
+public:
+    bool loadElfFile(Hart &hart, const std::string &filename);
+    bool allocateStack(Hart &hart, const memory::VirtAddr stackAddr, const size_t stackSize);
+    bool setupCmdArgs(Hart &hart, int argc, char **argv, char **envp);
+
+    OSHelper(const OSHelper& other) = delete;
+
+    static OSHelper* create() {
+        if (!instancePtr)
+            instancePtr = new OSHelper();
+
+        return instancePtr;
+    }
+
+    static void destroy() {
+        ASSERT(instancePtr != nullptr);
+        delete instancePtr;
+        instancePtr = nullptr;
+    }
+};
+
+};  // namespace RISCV
+
+#endif  // OS_HELPER_H
